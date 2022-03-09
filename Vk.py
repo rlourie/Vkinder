86090975ca0eb420f9dd72d025fbed1bd1894c4f
rlourie <super.sanek20142@yandex.ru>
2022-03-09 17:25:09 +0300

    raw2

--- Vk.py
+++ Vk.py
@@ -2,6 +2,7 @@ from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from Token import token
+from db import insert_info_bd, update_param, check_param_bd, check_all_params, connection
 from pprint import pprint
 
 vk_session = vk_api.VkApi(token=token)
@@ -27,49 +28,52 @@ def write_msg(user_id, message):
 
 def get_info(user_id):
     info = vk_session.method('users.get', {"user_ids": user_id, "fields": "bdate,sex,city,relation"})
-    result_dict = {'age': info[0].get('bdate'), 'sex': info[0].get('sex'), 'city': info[0].get('city'),
+    result_dict = {'age': info[0].get('bdate'), 'sex': info[0].get('sex'), 'city': info[0].get('city').get('id'),
                    'relation': info[0].get('relation')}
-    return result_dict # Получаем всю инфу по пользователю
+    for key, value in result_dict.items():
+        if value is None:
+            result_dict[key] = 0  # Обнуляем пустые поля
+    return result_dict  # Получаем всю инфу по пользователю
 
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             info = get_info(event.user_id)
-            if len(check_data(info)) != 0:
+            insert_info_bd(connection, event.user_id, info)
+            if check_all_params(connection, event.user_id) == 0:
                 write_msg(event.user_id, f"У тебя не заполнены данные по профилю дополни информацию о себе:")
-                if info.get("age") is None:
+                if check_param_bd(connection, event.user_id, 'age') == 0:
                     write_msg(event.user_id, f"Введи свой возраст:")
-                    age = -1
-                    while info['age'] is None:
+                    while check_param_bd(connection, event.user_id, 'age') == 0:
                         response = vk_session.method('messages.getConversations',
                                                      {"offset": 0, "count": 1, "filter": 'unanswered'})
                         if len(response.get("items")) != 0:
-                            info['age'] = response.get("items")[0].get('last_message').get("text")
-                            print(info.get("age"))
-                if info.get("sex") is None:
+                            update_param(connection, event.user_id, 'age',
+                                         response.get("items")[0].get('last_message').get("text"))
+                if check_param_bd(connection, event.user_id, 'sex') == 0:
                     write_msg(event.user_id, f"Введи своё семейное положение:")
-                    while info['sex'] is None:
+                    while check_param_bd(connection, event.user_id, 'sex') == 0:
                         response = vk_session.method('messages.getConversations',
                                                      {"offset": 0, "count": 1, "filter": 'unanswered'})
                         if len(response.get("items")) != 0:
-                            info['sex'] = response.get("items")[0].get('last_message').get("text")
-                            print(info["relation"])
-                if info.get("city") is None:
+                            update_param(connection, event.user_id, 'sex',
+                                         response.get("items")[0].get('last_message').get("text"))
+                if check_param_bd(connection, event.user_id, 'city') == 0:
                     write_msg(event.user_id, f"Введи своё семейное положение:")
-                    while info['city'] is None:
+                    while check_param_bd(connection, event.user_id, 'city') == 0:
                         response = vk_session.method('messages.getConversations',
                                                      {"offset": 0, "count": 1, "filter": 'unanswered'})
                         if len(response.get("items")) != 0:
-                            info['city'] = response.get("items")[0].get('last_message').get("text")
-                            print(info["city"])
-                if info.get("relation") is None:
+                            update_param(connection, event.user_id, 'city',
+                                         response.get("items")[0].get('last_message').get("text"))
+                if check_param_bd(connection, event.user_id, 'relation') == 0:
                     write_msg(event.user_id, f"Введи своё семейное положение:")
-                    while info['relation'] is None:
+                    while check_param_bd(connection, event.user_id, 'relation') == 0:
                         response = vk_session.method('messages.getConversations',
                                                      {"offset": 0, "count": 1, "filter": 'unanswered'})
                         if len(response.get("items")) != 0:
-                            info['relation'] = response.get("items")[0].get('last_message').get("text")
-                            print(info["relation"])
+                            update_param(connection, event.user_id, 'relation',
+                                         response.get("items")[0].get('last_message').get("text"))
             else:
                 write_msg(event.user_id, f"У тебя заполнены данные по профилю")
--- db.py
+++ db.py
@@ -2,22 +2,47 @@ from pprint import pprint
 import sqlalchemy
 
 
-def insert_info_bd(user_id, result_dict):
-    db = 'postgresql://azvezdin:12345@localhost:5432/vkinder'
-    engine = sqlalchemy.create_engine(db)
-    connection = engine.connect()
-    user_id = 1
-    sel = connection.execute(f'''
+def insert_info_bd(connection_db, user_id, result_dict):
+    sel = connection_db.execute(f'''
     SELECT vk_id FROM vk_users
     WHERE vk_id = '{user_id}'
     ''').fetchall()
     if len(sel) == 0:
-        connection.execute(f'''
+        connection_db.execute(f'''
         INSERT  INTO vk_users (vk_id,vk_age,vk_sex,vk_city,vk_relation)
         VALUES ({user_id},{result_dict.get('age')},{result_dict.get('sex')},{result_dict.get('city')},{result_dict.get('relation')})
+        ''')
+        return 1  # Заполнение произошло
+    return 0  # Заполнение не произошло
+
+
+def update_param(connection_db, user_id, param, data):
+    param = 'vk_' + param
+    sel = connection_db.execute(f'''
+    UPDATE vk_users SET {param}='{data}'
+    WHERE vk_id = {user_id}
+    ''')
+
+
+def check_param_bd(connection_db, user_id, param):
+    param = 'vk_' + param
+    sel = connection_db.execute(f'''
+        SELECT {param} FROM vk_users
+        WHERE vk_id = '{user_id}'
         ''').fetchall()
+    if sel[0][0] == 0 or sel[0][0] == '0':
+        return 0  # Поле незаполнено
+    return 1  # Поле заполнено
 
-    return 0
+
+def check_all_params(connection_bd, user_id):
+    data = ['age', 'sex', 'city', 'relation']
+    for param in data:
+        if check_param_bd(connection_bd, user_id, param) == 0:
+            return 0
+    return 1
 
 
-insert_info_bd(134, {'age': 18, 'sex': 1, 'city': 1, 'relation': 1})
+db = 'postgresql://azvezdin:12345@localhost:5432/vkinder'
+engine = sqlalchemy.create_engine(db)
+connection = engine.connect()
